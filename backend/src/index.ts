cb149adf1feb4887b318dc40bc5add9f30a241b7
giovanni <giovannidealmeidamartins@gmail.com>
2024-04-27 12:10:13 -0300

    fix: add backend and database modules, perhaps i forgot to merge them

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(router);
 
